d519df098785ac4a5608c068a71d24e44991231d
Bobby Steinbach <steinbach.rj@gmail.com>
2019-04-13 16:54:55 -0400

    adds Spring Boot based camunda BPMN engine + docker configuration

---
+++ antifraud-bpmn/antifraud/src/main/java/com/labs/meanpug/bpm/antifraud/AntifraudProcessApplication.java
@@ -0,0 +1,11 @@
+package com.labs.meanpug.bpm.antifraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntifraudProcessApplication {
+	public static void main(String... args) {
+		SpringApplication.run(AntifraudProcessApplication.class, args);
+	}
+}
